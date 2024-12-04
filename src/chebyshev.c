c3daadf7ee46cc3c9085f61d9eb3e50eddc1a87f
thamada <thamada@users.noreply.github.com>
2024-12-05 01:42:37 +0900

    update chebychev.c

--- src/chebyshev.c
+++ src/chebyshev.c
@@ -4,18 +4,36 @@
 #define N 1  // チェビシェフ多項式の次数
 
 // チェビシェフ多項式の計算関数
+/**
 void chebyshev_coefficients(double (*f)(double), double a, double b, int n, double* coeff) {
     for (int k = 0; k <= n; k++) {
         double sum = 0.0;
-        for (int i = 0; i < n; i++) {
-            double x_i = cos(M_PI * (2 * i + 1) / (2.0 * n));
+        for (int i = 0; i <= n+1; i++) {
+            double x_i = cos(M_PI * (2 * i + 1) / (2.0 * (n + 1)));
             double x_mapped = 0.5 * (b - a) * (x_i + 1) + a;
-            sum += f(x_mapped) * cos(k * M_PI * (2 * i + 1) / (2.0 * n));
+            sum += f(x_mapped) * cos(k * M_PI * (2 * i + 1) / (2.0 * (n + 1)));
         }
-        coeff[k] = (2.0 / n) * sum;
+        coeff[k] = (2.0 / (n + 1)) * sum;
     }
     coeff[0] *= 0.5;  // 最初の係数は半分にする
 }
+*/
+
+void chebyshev_coefficients(double (*f)(double), double a, double b, int n, double* coeff) {
+    for (int k = 0; k <= n; k++) {
+        double sum = 0.0;
+        int ni = 1024;
+        for (int i = 0; i < ni; i++) {
+          double x_i = cos(M_PI * (2 * i + 1) / (2.0 * ni));
+          double x_mapped = 0.5 * (b - a) * (x_i + 1) + a;
+          sum += f(x_mapped) * cos(k * M_PI * (2 * i + 1) / (2.0 * ni));
+        }
+        coeff[k] = (2.0 / (ni + 1)) * sum;
+    }
+    coeff[0] *= 0.5;  // 最初の係数は半分にする
+}
+
+
 
 // チェビシェフ補間の評価関数
 double chebyshev_interpolate(double x, double a, double b, int n, double* coeff) {
@@ -36,7 +54,8 @@ double chebyshev_interpolate(double x, double a, double b, int n, double* coeff)
 // 対象の関数
 double func(double x) {
   //   return log(1 + x);
-  return x*(x-0.25)*(x-0.5)*(x-1.0);
+  //  return x*(x-0.25)*(x-0.5)*(x-1.0);
+  return x*(x-0.5)*(x-1.0);
 }
 
 int main() {
@@ -45,17 +64,18 @@ int main() {
     double coeff[N + 1];
 
     // チェビシェフ係数を計算
-    chebyshev_coefficients(func, a, b, N+1, coeff);
+    chebyshev_coefficients(func, a, b, N, coeff);
 
     // 結果を表示
-    printf("チェビシェフ補間の係数:\n");
+    fprintf(stderr, "チェビシェフ補間の係数:\n");
     for (int i = 0; i <= N; i++) {
-        printf("c[%d] = %f\n", i, coeff[i]);
+        fprintf(stderr, "c[%d] = %f\n", i, coeff[i]);
     }
+
 
     // 任意の点で補間値を計算
     for (double x = a; x <= b; x += 1.0/1024.0) {
-      double z_cheby = chebyshev_interpolate(x, a, b, N+1, coeff);
+      double z_cheby = chebyshev_interpolate(x, a, b, N, coeff);
       double z_truth = func(x);
 
       double diff = fabs(z_truth - z_cheby);
--- src/plot.sh
+++ src/plot.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env gnuplot
-plot "tmp.txt" using 1:2 w lines, \
-     "tmp.txt" using 1:3 w lines, \
-     "tmp.txt" using 1:4 w lines \
+
+set grid
+
+plot "/tmp/log.txt" using 1:2 w lines, \
+     "/tmp/log.txt" using 1:3 w lines, \
+     "/tmp/log.txt" using 1:4 w lines
 
 pause -1
 
